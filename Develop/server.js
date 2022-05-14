7a75d2a6d9d79c9c5aa5906e65767d1e86f40cee
Tori <torihendricks927@yahoo.com>
2022-05-14 17:12:50 -0400

    setup express use and path

--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
