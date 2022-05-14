6f36802baa73e442de6a2268cd3f22f27b28f276
Tori <torihendricks927@yahoo.com>
2022-05-14 17:18:19 -0400

    added GET/POST for notes

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,21 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// GET for notes.html file
+app.get('/api/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// POST for notes.html
+app.post('/api/notes', (req,res) => {
+    res.json('post!');
+});
+
+
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
