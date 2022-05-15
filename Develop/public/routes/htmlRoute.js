e3cd27438ac1572134823418b135b80fb9a91159
Tori <torihendricks927@yahoo.com>
2022-05-15 13:59:44 -0400

    finalized route call

--- Develop/public/routes/htmlRoute.js
+++ Develop/public/routes/htmlRoute.js
@@ -4,11 +4,12 @@ const router = require('express').Router();
 
 // Moved to new route module for homework requirements
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, './notes.html'));
 });
 
+// To return or start homepage
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 module.exports = router;
