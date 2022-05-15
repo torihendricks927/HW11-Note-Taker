20bbb60bf89c3c669b5b0fb9a62b094620603676
Tori <torihendricks927@yahoo.com>
2022-05-15 13:09:48 -0400

    moving get route to separate module for HW requirements

--- Develop/server.js
+++ Develop/server.js
@@ -9,16 +9,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// GET for notes.html file
-app.get('/api/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+// // GET for notes.html file
+// app.get('/notes', (req,res) => {
+//     res.sendFile(path.join(__dirname, '/public/notes.html'));
+// });
+
+// // GET for index.html file
+// app.get('*', (req,res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'));
+// });
+
+// // GET for notes
+// app.get('/api/notes', (req,res) => {
+//     res.sendFile
+// })
 
 // POST for notes.html
-app.post('/api/notes', (req,res) => {
-    res.json('post!');
-});
+// app.post('/api/notes', (req,res) => {
+//     res.json('post!');
+// });
 
 
-
+// starts PORT server
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
