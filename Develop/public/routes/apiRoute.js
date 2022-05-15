1b2d8a656445c5bf1c6b055dda0c9963f104f0ad
Tori <torihendricks927@yahoo.com>
2022-05-15 14:06:53 -0400

    moved get and post to this route

--- Develop/public/routes/apiRoute.js
+++ Develop/public/routes/apiRoute.js
@@ -1,1 +1,16 @@
+const path = require('path');
 const router = require('express').Router();
+
+// GET call
+router.get('/notes', (req,res) => {
+    res.json('Get Completed');
+});
+
+// POST for notes.html
+router.post('/notes', (req,res) => {
+    res.json('Post Completed');
+});
+
+
+
+module.exports = router;
